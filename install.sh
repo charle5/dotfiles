21a4c9ddb4c1d284217a9c9ca28af793ed0bde81
charle5 <devin.holloway+charle5@gmail.com>
2014-01-08 20:13:27 -0800

    did stuff with .oh-my-zsh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "symlinking dotfiles to home dir"
-ln -sf ~/.dotfiles/.emacs ~/.emacs
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
-ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/.dotfiles/.tmuxinator ~/.tmuxinator
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
