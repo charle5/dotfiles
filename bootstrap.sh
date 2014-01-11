24d8f8d4597821f2864287161e47848c3234e18f
charle5 <devin.holloway+charle5@gmail.com>
2014-01-10 23:57:38 -0800

    added file type exclusions to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@ git pull origin master
 
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-	--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+	--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms \
+	--exclude "*~" --exclude "#*" . ~
     source ~/.zshrc
 }
 
