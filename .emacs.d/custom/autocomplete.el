16bceff28ca463d060159fe5745c35d0d4689125
charle5 <devin.holloway+charle5@gmail.com>
2014-01-26 15:45:38 -0800

    added and fixed stuff

--- .emacs.d/custom/autocomplete.el
+++ .emacs.d/custom/autocomplete.el
@@ -1,3 +1,5 @@
+;; from here https://github.com/auto-complete/auto-complete
+
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories
 	     "~/.emacs.d/.cask/24.3.1/elpa/auto-complete-20131128.233/dict/")
